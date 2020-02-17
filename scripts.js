5dd0bf4bba1296284a1c92aca0c86f8129dfd5c8
NicholasPierce1 <42748558+NicholasPierce1@users.noreply.github.com>
2020-02-17 12:52:05 -0600

    tip configured to be relative to subtotal

--- scripts.js
+++ scripts.js
@@ -19,14 +19,17 @@ document.getElementById("calculateTotal").addEventListener("click", async () =>{
     const tipInput = parseFloat(tipTotalInput.value);
     
     // local memeber to hold grand total and tax total
+    
+    // tip total (20% -- .2 * subtotal)
+    const tipTotal = (tipInput / 100) * mealInput;
     const taxTotal = mealInput * taxTotalCoeffiecient;
-    const grandMealTotal = taxTotal + tipInput + mealInput;
+    const grandMealTotal = taxTotal + tipTotal + mealInput;
     
     console.log(`mealTotalInput: ${mealTotalInput.value}`);
     console.log(`tip total input: ${tipTotalInput.value}`);
     
     // updates tip total with meal total * tip input / 100
-    tipTotalDisplay.innerHTML = `${tipTotalDisplayPrefix} ${Math.round(tipInput * 100)/100}`;
+    tipTotalDisplay.innerHTML = `${tipTotalDisplayPrefix} ${Math.round(tipTotal * 100)/100}`;
     
     
     // updates tax total with mealTotal * tax rate
